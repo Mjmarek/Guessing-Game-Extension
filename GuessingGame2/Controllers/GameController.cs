e7024c8ff7654269c4167071d3c4e72ee531fccd
Mjmarek <monicamarek24@gmail.com>
2017-11-15 20:34:09 -0500

    Initial commit

---
+++ GuessingGame2/Controllers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using GuessingGame2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessingGame2.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            Session["answer"] = new Random().Next(1, 26);
+            return View();
+        }
+
+        private bool GuessWasCorrect(int guess) =>
+            guess == (int)Session["answer"];
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(GameModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               ViewBag.Win = GuessWasCorrect(model.Guess);
+            }
+            return View(model);
+        }
+
+        //if (answer > guess)
+        //    Sorry, your answer is too low. Please try again.
+
+        //if (answer < guess)
+        //    Sorry, your answer is too high. Please try again.
+
+        //Stretch Goals
+        //tell the user if guess is too high or too low
+        //tell the user if they've already guessed that number
+    }
+}
---
+++ GuessingGame2/Models/GameModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame2.Models
+{
+    public class GameModel
+    {
+        [Required]
+        [MaxLength (10, ErrorMessage = "Please choose a name less than 10 characters long.")]
+        [Display(Name = "Player Name")]
+        public string PlayerName { get; set; }
+
+        [Range(1, 25, ErrorMessage = "Hint: The number will be between 1 and 25.")]
+        public int Guess { get; set; }
+    }
+}
