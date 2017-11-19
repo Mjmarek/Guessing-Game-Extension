3405006cbb447bf243f55256d936caa354405bc2
Mjmarek <monicamarek24@gmail.com>
2017-11-19 11:48:04 -0500

    Prior to migrating database

---
+++ GuessingGame2.ServicesTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuessingGame2.ServicesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
